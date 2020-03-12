b29f098c5688bbe5c557d2ad3e695a6485baef46
naty4393 <naty4393@gmail.com>
2020-03-12 02:48:00 -0500

    update

--- router/components.js
+++
@@ -1,9 +0,0 @@
-import Home from '../../../home.js';
-import Tarea1 from '../homeworks/homework-1.js';
-import Tarea2 from '../homeworks/homework-2.js';
-
-export const component = {
-  home: Home,
-  tarea1: Tarea1,
-  tarea2: Tarea2,
-};
